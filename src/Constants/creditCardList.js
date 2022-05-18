4adadc711ff8038a796c3dc6ca2cdb648c1593ee
Theenadayalan Kaliamurthy <puduvai.theena@gmail.com>
2022-05-19 02:58:16 +0530

    changed year format

--- src/Constants/creditCardList.js
+++ src/Constants/creditCardList.js
@@ -3,46 +3,46 @@ const creditCardList = [
         id: '1',
         name: 'Theena',
         digits: '1234',
-        expiry: '04/2020',
+        expiry: '04/20',
         background: '1'
     }
     , {
         id: '2',
         name: 'Dayalan',
         digits: '8463',
-        expiry: '02/2027',
+        expiry: '02/27',
         background: '5'
     }
     , {
         id: '3',
         name: 'Saravanan',
         digits: '5678',
-        expiry: '05/2021',
+        expiry: '05/21',
         background: '3'
     }, 
     // {
     //     id: '4',
     //     name: 'Kumar',
     //     digits: '9012',
-    //     expiry: '06/2022',
+    //     expiry: '06/22',
     //     background: '4'
     // }, {
     //     id: '5',
     //     name: 'Karthik',
     //     digits: '3456',
-    //     expiry: '07/2023',
+    //     expiry: '07/23',
     //     background: '5'
     // }, {
     //     id: '6',
     //     name: 'Chandan',
     //     digits: '7890',
-    //     expiry: '08/2024',
+    //     expiry: '08/24',
     //     background: '6'
     // }, {
     //     id: '7',
     //     name: 'Srinivas',
     //     digits: '1234',
-    //     expiry: '09/2025',
+    //     expiry: '09/25',
     //     background: '7'
     // }
 ]
